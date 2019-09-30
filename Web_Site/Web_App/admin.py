ff06e581b1d653fe550727f93d2c8701600b063c
Suvoparno Banerjee <Suvoparno@Suvoparnos-MacBook-Pro.local>
2019-09-30 19:52:10 +0530

    v4

--- Web_Site/Web_App/admin.py
+++ Web_Site/Web_App/admin.py
@@ -6,8 +6,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Colleges
 from .models import Course
-from .models import Chat
 
 admin.site.register(Colleges)
 admin.site.register(Course)
-admin.site.register(Chat)
+
+  
--- Web_Site/Web_App/models.py
+++ Web_Site/Web_App/models.py
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
 class Colleges(models.Model):
+     def __str__(self):
+          return 'College Name: ' + self.college
      college = models.CharField('College Name', max_length=120)
      courses = models.CharField('Courses Available', max_length=120)
      address = models.CharField(max_length=120)
      marks = models.IntegerField(default='50')
      phone = models.IntegerField(null=True)
      fees = models.TextField(blank=True)
+     fields = ('college', 'address', 'marks', 'phone', 'fees')
      
 class Course(models.Model):
      name = models.CharField('Course Name', max_length=120)
      exam = models.CharField('Exam Required', max_length=120, null=True)
-class Chat(models.Model):
-     name = models.CharField('Course Name', max_length=120)
-     marks = models.IntegerField(default='50')
+
+class Blog(models.Model):
+   title = models.CharField(max_length=100, unique=True)
+   slug = models.SlugField(max_length=100, unique=True)
+   body = models.TextField()
+   posted = models.DateField(db_index=True, auto_now_add=True)
+   category = models.ForeignKey('Web_App.Category', on_delete=models.PROTECT)
+
+class Category(models.Model):
+     title = models.CharField(max_length=100, db_index=True)     
+     slug = models.SlugField(max_length=100, db_index=True)
+
+#     def __str__(self):
+#         return self.title
